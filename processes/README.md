24dea201411fb30c00a09b8638b38b4fa902517d
Yuri Solyanik <jura.s.n@gmail.com>
2024-02-16 09:03:58 +0500

    Refactoring after code review

--- processes/README.md
+++ processes/README.md
@@ -5,7 +5,7 @@
 ```elixir
 iex --sname node1@localhost -S mix
 
-{:ok, p} = Processes.Echo.start()
+{:ok, p} = Processes.Echo.start_link()
 ```
 
 ## Node 2
@@ -16,6 +16,5 @@ iex --sname node2@localhost -S mix
 Node.connect(:node1@localhost)
 Node.list([:this, :visible])
 
-p = :global.whereis_name(:echo_server)
-GenServer.call(p, :ping)
+GenServer.call({:global, :echo_server}, :ping)
 ```
--- processes/lib/processes/echo.ex
+++ processes/lib/processes/echo.ex
@@ -1,23 +1,26 @@
 defmodule Processes.Echo do
   @moduledoc """
   Модуль работающий как эхо сервер.
-  Получает сообщение :ping и высылает обратно {:pong, node()}.
+  Получает синхронное сообщение :ping и высылает обратно {:pong, node()}.
   """
 
   use GenServer
 
   require Logger
 
-  def start() do
+  @doc "Запускает процесс с глобальным именем `:echo_server`"
+  def start_link() do
     GenServer.start_link(__MODULE__, nil, name: {:global, :echo_server})
   end
 
   @impl true
+  @doc false
   def init(state) do
     {:ok, state}
   end
 
   @impl true
+  @doc false
   def handle_call(:ping, _from, state) do
     {:reply, {:pong, node()}, state}
   end
